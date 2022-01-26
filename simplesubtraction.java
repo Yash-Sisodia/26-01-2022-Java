8c9500140a2e36900470670a751938ef27ba5c53
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-01-26 15:23:59 +0530

    diff

---
+++ simplesubtraction.java
@@ -0,0 +1,10 @@
+public class simplesubtraction {
+    public static void main(String[] args) {
+        int a=50;
+        int b=25;
+        int diff =a-b;
+        System.out.print(diff);
+        
+    }
+    
+}
