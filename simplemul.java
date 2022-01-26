d80688c3c8f1be265727dd7da955aa13afa65026
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-01-26 15:29:25 +0530

    mul

---
+++ simplemul.java
@@ -0,0 +1,10 @@
+public class simplemul {
+    public static void main(String[] args)
+    {
+        int a =12;
+        int b =2;
+        int mul =a*b;
+        System.out.print(mul);
+    }
+    
+}
