f775a389208d659fe06b5d1d8c0b99dbad0aedfa
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-01-26 16:53:37 +0530

    twovariable

---
+++ twovariable.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class twovariable {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int sum = d+e;
+
+        System.out.print(sum);
+
+
+    }
+}
+
+/* input = 100
+100
+output= 200 */
