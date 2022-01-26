50bb2b367188cd9b39c3886ed9ccb896ad9e05e1
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-01-26 15:16:17 +0530

    sum

---
+++ simplesum.java
@@ -0,0 +1,11 @@
+public class simplesum 
+{
+    public static void main(String[] args)
+     {
+        int a =25;
+        int b =25;
+        int sum= a+b;
+        System.out.println(sum); 
+    }
+    
+}
