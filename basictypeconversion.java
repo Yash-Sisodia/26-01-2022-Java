5cbfc2722882bad0e4c6b07bcf831e86c41080ba
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-01-27 13:42:29 +0530

    msg

---
+++ basictypeconversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class basictypeconversion {
+ public static void main(String[] args)
+  {
+     Scanner scn = new scanner(System.in);
+     float num = input.nextFloat();
+     System.out.print(num);
+ }
+    
+}
