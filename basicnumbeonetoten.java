c136df9911279565e2baba56126a2ac10b972f91
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-01-28 13:50:21 +0530

    one to ten

---
+++ basicnumbeonetoten.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class basicnumbeonetoten {
+    public static void main(String[] args) {
+        // counter = counter+1 ; or we can write as counter++
+        for(int counter=1 ; counter<11 ; counter++) {
+            System.out.print(counter+ " ");
+        }
+    }
+    
+}
