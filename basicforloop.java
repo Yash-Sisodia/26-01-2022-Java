6106acd141d9257f0a58a9377fa04696e46b12d5
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-01-28 13:25:31 +0530

    basicforloop

---
+++ basicforloop.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class basicforloop {
+    public static void main(String[] args) {
+        for(int count =0 ; count<100 ; count = count+1) {
+            System.out.print("Hello Yash Sisodia \n");
+        }
+    }
+    
+}
