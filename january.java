e43a0ad23bcd3b142132c515e94bedede7c9080b
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-01-28 23:07:32 +0530

    Jan

---
+++ january.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class january {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number n = ");
+        
+        int n = sc.nextInt();
+
+        switch(n) {
+            case 1 : System.out.println("January");
+            break;
+            case 2 : System.out.println("Feb");
+            break;
+            case 3 : System.out.println("March");
+            break;
+            case 4 : System.out.println("April");
+            break;
+            case 5 : System.out.println("May");
+            break;
+            case 6 : System.out.println("June");
+            break;
+            case 7 : System.out.println("July");
+            break;
+            case 8 : System.out.println("August");
+            break;
+            case 9 : System.out.println("September");
+            break;
+            case 10 : System.out.println("Oct");
+            break;
+            case 11 : System.out.println("Nov");
+            break;
+            case 12 : System.out.println("Dec");
+            break;
+
+            default : System.out.println("Entered number is invalid");
+
+        }
+    }
+    
+}
