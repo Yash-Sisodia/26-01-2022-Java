ff47644505770b95d5a04b9cafa1f4aca84d5a1a
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-02-11 14:34:29 +0530

    Hackerrank Third question

---
+++ hackerquestionthree.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class hackerquestionthree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = Integer.parseInt(sc.nextLine());
+        Double choice = sc.nextDouble();
+        sc.nextLine();
+        String name = sc.nextLine();
+
+        System.out.println("String :" +name);
+        System.out.println("Double :" +choice);
+        System.out.println("Integer :" +n1);
+    }
+}
